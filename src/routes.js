b33cfa5042732c9eb9de52999c080c2593bdaba9
Joziélio Santiago <jozieliosantiago@gmail.com>
2019-07-22 19:38:19 -0300

    editorconfig added end idetention applicated

--- src/routes.js
+++ src/routes.js
@@ -8,15 +8,15 @@ import New from './pages/New';
 import logo from './assets/logo.png';
 
 export default createAppContainer(
-    createStackNavigator({
-        Feed,
-        New,
-    }, {
-        defaultNavigationOptions: {
-            headerTintColor: '#000',
-            headerTitle: <Image style={{ marginHorizontal: 20 }} source={logo} />,
-            headerBackTitle: null,
-        },
-        mode: 'modal'
+  createStackNavigator({
+    Feed,
+    New,
+  }, {
+      defaultNavigationOptions: {
+        headerTintColor: '#000',
+        headerTitle: <Image style={{ marginHorizontal: 20 }} source={logo} />,
+        headerBackTitle: null,
+      },
+      mode: 'modal'
     })
 );
